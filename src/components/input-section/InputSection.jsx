beb18366adeb38e0f11cc41c92811c8a7423cfcb
AmirMasoud Jafari <amirmasoodjafari1677@gmail.com>
2022-05-03 12:11:32 +0430

    finish all mui inputs component

--- src/components/input-section/InputSection.jsx
+++ src/components/input-section/InputSection.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const InputSection = ({ children, title }) => {
   return (
-    <div className="border-2 border-red-700 relative">
+    <div className="border-2 border-[#027FFF] relative">
       <h2 className="bg-white p-4 absolute -top-8 text-xl font-semibold">
         {title}
       </h2>
--- src/components/inputs/mui-inputs/MuiInputs.jsx
+++ src/components/inputs/mui-inputs/MuiInputs.jsx
@@ -1,7 +1,114 @@
+import {
+  Box,
+  FormControl,
+  IconButton,
+  Input,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+  TextField,
+} from '@mui/material';
+import { AccountCircle, VisibilityOff, Visibility } from '@mui/icons-material';
 import React from 'react';
 
 const MuiInputs = () => {
-  return <div>MuiInputs</div>;
+  const [values, setValues] = React.useState({
+    password: '',
+    showPassword: false,
+  });
+
+  const handleClickShowPassword = () => {
+    setValues({
+      ...values,
+      showPassword: !values.showPassword,
+    });
+  };
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-6 md:grid-cols-6 xl:grid-cols-6 gap-6">
+      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+      <TextField id="filled-basic" label="Filled" variant="filled" />
+      <TextField id="standard-basic" label="Standard" variant="standard" />
+      <TextField
+        error
+        id="filled-error-helper-text"
+        label="Error"
+        defaultValue="Hello World"
+        helperText="Incorrect entry."
+        variant="filled"
+      />
+      <TextField
+        error
+        id="standard-error"
+        label="Error"
+        defaultValue="Hello World"
+        variant="standard"
+      />
+      <TextField
+        error
+        id="standard-error-helper-text"
+        label="Error"
+        defaultValue="Hello World"
+        helperText="Incorrect entry."
+        variant="standard"
+      />
+      <TextField
+        id="outlined-multiline-static"
+        label="Multiline"
+        multiline
+        rows={4}
+        defaultValue="Default Value"
+      />
+      <FormControl variant="standard">
+        <InputLabel htmlFor="input-with-icon-adornment">
+          With a start adornment
+        </InputLabel>
+        <Input
+          id="input-with-icon-adornment"
+          startAdornment={
+            <InputAdornment position="start">
+              <AccountCircle />
+            </InputAdornment>
+          }
+        />
+      </FormControl>
+      <Box sx={{ display: 'flex', alignItems: 'start' }}>
+        <AccountCircle sx={{ color: 'action.active', mr: 1, my: 3 }} />
+        <TextField id="input-with-sx" label="With sx" variant="standard" />
+      </Box>
+      <TextField label="Outlined secondary" color="secondary" focused />
+      <TextField
+        label="With normal TextField"
+        id="outlined-start-adornment"
+        InputProps={{
+          startAdornment: <InputAdornment position="start">kg</InputAdornment>,
+        }}
+      />
+      <FormControl variant="outlined">
+        <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+        <OutlinedInput
+          id="outlined-adornment-password"
+          type={values.showPassword ? 'text' : 'password'}
+          endAdornment={
+            <InputAdornment position="end">
+              <IconButton
+                aria-label="toggle password visibility"
+                onClick={handleClickShowPassword}
+                onMouseDown={handleMouseDownPassword}
+                edge="end"
+              >
+                {values.showPassword ? <VisibilityOff /> : <Visibility />}
+              </IconButton>
+            </InputAdornment>
+          }
+          label="Password"
+        />
+      </FormControl>
+    </div>
+  );
 };
 
 export default MuiInputs;
